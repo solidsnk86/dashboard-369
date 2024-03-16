6c8251a870f2ad50c1dcf1a91f9c2c11ebff846c
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-16 18:08:55 -0300

    update next team customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import { Metadata } from "next"
+import { fetchCustomers } from "@/app/lib/data"
+
+export const metadata: Metadata = {
+    title: 'Clientes | Atlas'
+}
+
+export default async function Page() {
+    const [customer] = await Promise.all([
+        fetchCustomers()
+    ])
+    return <p>Página de Clientes</p>
 }

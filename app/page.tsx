9a027a241fac36d22feb0128200015491a0fa596
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-03-18 00:28:20 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Bienvenidos a Atlas!</strong> Este es un ejemplo de facturador del curso de{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-400">Next.js</a>, creado por Vercel.
+            <a href="https://nextjs.org/learn/" className="text-blue-400" title='Ir al Curso de Next.js'>Next.js</a>, creado por Vercel.
           </p>
           <Link
             href="/login"
